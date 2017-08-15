c165abe7c7298d26a7ba07b9d07cf43a8c37a155
BU Spark! <buspark@dhcp-wifi-8021x-155-41-34-54.bu.edu>
2017-08-14 21:54:24 -0400

    Fixed hero text, and added typing animation in JS

---
+++ js/main.js
@@ -0,0 +1,47 @@
+$(document).ready(function() {
+  
+  //Author: Lucas Watson <http://lkwatson.me>
+  
+  var wordList = ["ignite ideas.","explore projects.","launch projects."];
+  var index = 0; //incrementer for Hero text
+  var currentWord = "";
+  var wordTime = 5000; //How long each word is shown for
+  
+  function changeWord() {
+    index = (index >= wordList.length-1) ? 0 : index+1;
+    currentWord = wordList[index];
+    console.log(currentWord)
+  }
+  
+  var lettersShow = 0; //Store how many letters are currently displayed
+  var timeElapsed = 0; //How long the current clock loop has gone
+  var flipped = false; //Have we completed one typing of the word, and will now delete it
+  var letterTime = 80; //how long to show each letter
+  
+  function clock() {
+    timeElapsed = timeElapsed + letterTime;
+    if (!flipped) {
+      lettersShow = lettersShow + 1;
+      flipped = (lettersShow >= currentWord.length)
+    }else{
+      if ( ((wordTime - timeElapsed)/letterTime) < currentWord.length ) {
+        lettersShow = lettersShow - 1;
+        
+        if (lettersShow == 0) {
+          flipped = false;
+          timeElapsed = 0;
+          changeWord();
+        }
+      }
+    }
+
+    $('#anim-text').text(function(i,t) {
+      return currentWord.substr(0,lettersShow);
+    })
+  }
+  
+  changeWord(); //Run once
+
+  setInterval(function(){clock()}, letterTime); //clock tick to do each letter type
+  
+});
